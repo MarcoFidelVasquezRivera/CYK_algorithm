c814674840137794678a7d25149a112dc0d5e466
Marco Vasquez <macroxx120@gmail.com>
2021-11-07 18:27:18 -0500

    created IGIC interface and GIC class

---
+++ CYK_Algorithm/Model/GIC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CYK_Algorithm.Model
+{
+    public class GIC : IGIC
+    {
+        private IList<string> variables;
+        private IList<IList<string>> productions;
+
+        public GIC(IList<String> variables, IList<IList<string>> productions)
+        {
+            this.variables = variables;
+            this.productions = productions;
+        }
+
+        public bool CYK()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
